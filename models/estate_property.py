d20fb519cb606e7f2331703a113477cb47cc5f93
JLOM <jlom@odoo.com>
2024-10-21 15:33:26 -0600

    Finished Chapter 03 - Before exercise 02

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,6 +1,14 @@
-from odoo import models
+from odoo import fields, models
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Property"
+
+    name = fields.Char()
+    garden_orientation = fields.Selection([
+        ("north", "North"),
+        ("south", "South"),
+        ("east", "East"),
+        ("west", "West"),
+    ])
