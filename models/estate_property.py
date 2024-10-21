9079625e3bccd936c737a322a8416d145a5c2ff7
JLOM <jlom@odoo.com>
2024-10-21 15:41:51 -0600

    Finished Chapter 03 - After exercise 02

--- models/estate_property.py
+++ models/estate_property.py
@@ -6,6 +6,17 @@ class EstateProperty(models.Model):
     _description = "Property"
 
     name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
     garden_orientation = fields.Selection([
         ("north", "North"),
         ("south", "South"),
