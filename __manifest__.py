a2b1afc02ba75a99e5a8e00927cced0c44a6df4f
JLOM <jlom@odoo.com>
2025-04-07 15:39:25 -0600

    Finished Chapter 05 - Before exercise 01

--- __manifest__.py
+++ __manifest__.py
@@ -5,6 +5,8 @@
     ],
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/real_estate_menus.xml",
     ],
     "application": True,
 }
