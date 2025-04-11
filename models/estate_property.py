960b7fa7528e07bcdda6bf5494095dd8247085bf
JLOM <jlom@odoo.com>
2025-04-11 11:44:01 -0600

    Finished Chapter 05 - After exercise 02

--- models/estate_property.py
+++ models/estate_property.py
@@ -8,9 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(default=lambda self: fields.Date.add(fields.Date.today(), months=3))
+    date_availability = fields.Date(
+        copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
