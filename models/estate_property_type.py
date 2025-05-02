c21d4f40ba10dff3b6fa2146d360a9d3284077f9
JLOM <jlom@odoo.com>
2025-05-02 12:24:43 -0600

    Finished Chapter 11 - After exercise 02

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -4,8 +4,10 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Property Type"
+    _order = "sequence"
 
     _sql_constraints = [("name_uniq", "UNIQUE(name)", "A type with this name already exists!")]
 
+    sequence = fields.Integer(default=1, help="Order of property types. Lower appears first.")
     name = fields.Char(required=True)
     property_ids = fields.One2many(comodel_name="estate.property", inverse_name="property_type_id")
