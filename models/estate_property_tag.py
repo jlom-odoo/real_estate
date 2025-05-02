1b8af3707a25c9c32bc58e642062ea47a08d02ad
JLOM <jlom@odoo.com>
2025-05-02 12:06:28 -0600

    Finished Chapter 11 - Before exercise 02

--- models/estate_property_tag.py
+++ models/estate_property_tag.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Property Tag"
+    _order = "name"
 
     _sql_constraints = [("name_uniq", "UNIQUE(name)", "A tag with this name already exists!")]
 
