a38537390bc1b642799d718694b3b247849a7f7e
JLOM <jlom@odoo.com>
2025-05-14 15:28:55 -0600

    Finished Chapter 11

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models, _
 
 
 class EstatePropertyType(models.Model):
@@ -11,3 +11,22 @@ class EstatePropertyType(models.Model):
     sequence = fields.Integer(default=1, help="Order of property types. Lower appears first.")
     name = fields.Char(required=True)
     property_ids = fields.One2many(comodel_name="estate.property", inverse_name="property_type_id")
+    offer_ids = fields.One2many(comodel_name="estate.property.offer", inverse_name="property_type_id")
+    offer_count = fields.Integer(compute="_compute_offer_count")
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for offer in self:
+            offer.offer_count = len(offer.offer_ids)
+
+    def action_open_offer_ids(self):
+        self.ensure_one()
+        return {
+            "name": _("Offers"),
+            "type": "ir.actions.act_window",
+            "view_mode": "tree,form",
+            "res_model": "estate.property.offer",
+            # "target": "current",
+            "domain": [("property_type_id", "=", self.id)],
+            "context": {"default_property_type_id": self.id},
+        }
